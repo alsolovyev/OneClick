63bd33b7693bbf98fed88c1e29b50e950f6861db
Aleksey Solovyev <solovyev.a@icloud.com>
2023-01-01 11:48:11 +0300

    other: initial commit

---
+++ OneClick/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  OneClick
+//
+//  Created by Aleksey Solovyev on 01.01.2023.
+//
+
+import Foundation
---
+++ OneClick/OneClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneClickApp.swift
+//  OneClick
+//
+//  Created by Aleksey Solovyev on 01.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct OneClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
