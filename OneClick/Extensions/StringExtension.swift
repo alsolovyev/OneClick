41cb35a58ab64afccca82a48e19bb12e46e12f97
Aleksey Solovyev <solovyev.a@icloud.com>
2023-01-03 14:57:03 +0300

    feat: add return value to runScript

--- OneClick/Extensions/StringExtension.swift
+++ OneClick/Extensions/StringExtension.swift
@@ -9,9 +9,9 @@ import OSAKit
 
 
 extension String {
-    func runScript(_ asAdmin: Bool = false) -> Void {
+    func runScript(_ asAdmin: Bool = false) throws -> String {
         var source: String = "do shell script \"\(self)\""
-
+        
         if (asAdmin) {
             source += " with prompt \"OneClick\" with administrator privileges"
         }
@@ -20,6 +20,16 @@ extension String {
         process.arguments = ["-e", source]
         process.launchPath = "/usr/bin/osascript"
         process.standardInput = nil
-        process.launch()
+        
+        let outputPipe = Pipe()
+        process.standardOutput = outputPipe
+        
+        do {
+            try process.run()
+            let outputData = outputPipe.fileHandleForReading.readDataToEndOfFile()
+            return String(decoding: outputData, as: UTF8.self)
+        } catch {
+            throw OneClickError.ScriptFailed
+        }
     }
 }
---
+++ OneClick/ViewModels/LowPowerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LowPowerViewModel.swift
+//  OneClick
+//
+//  Created by Aleksey Solovyev on 03.01.2023.
+//
+
+import Foundation
