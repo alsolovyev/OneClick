41d5513c42e7d85a117ef9b25338411a398fac65
Aleksey Solovyev <solovyev.a@icloud.com>
2023-01-03 14:58:01 +0300

    feat: add low power mode
    A class to enable, disable and get status of the low power mode

--- OneClick/ViewModels/LowPowerViewModel.swift
+++ OneClick/ViewModels/LowPowerViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class LowPowerViewModel: ObservableObject {
+    @Published public var isEnabled: Bool = false
+    
+    init() {
+        isEnabled = getStatus()
+    }
+    
+    func getStatus() -> Bool {
+        do {
+            let output = try "pmset -g | grep lowpowermode".runScript()
+            return output.contains("1")
+        } catch {
+            return false
+        }
+    }
+    
+    func enable() -> Void {
+        do {
+            _ = try "pmset -a lowpowermode 1".runScript(true)
+            isEnabled = true
+        } catch { }
+    }
+    
+    func disable() -> Void {
+        do {
+            _ = try "pmset -a lowpowermode 0".runScript(true)
+            isEnabled = false
+        } catch { }
+    }
+}
--- OneClick/Views/ContentView.swift
+++ OneClick/Views/ContentView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var lowPower: LowPowerViewModel = LowPowerViewModel()
+    
     var body: some View {
         VStack {
             Button("Enter Full Screen") {
                 WindowManagerService.shared.enterFullScreen()
+            }
+            
+            Button("\(lowPower.isEnabled ? "Disable" : "Enable") low power mode") {
+                lowPower.isEnabled ? lowPower.disable() : lowPower.enable()
             }
         }
         .padding()
