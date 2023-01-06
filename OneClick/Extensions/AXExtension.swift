aff52a3444cb64fbaa5f8729ed686891f28534cf
Aleksey Solovyev <solovyev.a@icloud.com>
2023-01-06 16:10:29 +0300

    feat: add getValue to AXUIElement

--- OneClick/Extensions/AXExtension.swift
+++ OneClick/Extensions/AXExtension.swift
@@ -9,6 +9,15 @@ import Cocoa
 
 
 extension AXUIElement {
+    func getValue(_ attribute: NSAccessibility.Attribute) -> AnyObject? {
+        var value: AnyObject?
+        
+        let result = AXUIElementCopyAttributeValue(self, attribute.rawValue as CFString, &value)
+        guard result == .success else { return nil }
+        
+        return value
+    }
+    
     func setValue(_ attribute: NSAccessibility.Attribute, _ value: AnyObject) {
         AXUIElementSetAttributeValue(self, attribute.rawValue as CFString, value)
     }
