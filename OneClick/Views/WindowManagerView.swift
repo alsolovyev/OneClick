fb19b4f76493c6d075afaba8cb507817e2fc6a59
Aleksey Solovyev <solovyev.a@icloud.com>
2023-01-06 20:59:54 +0300

    feat: add horizontal scroll to WindowManagerView

--- OneClick/Views/WindowManagerView.swift
+++ OneClick/Views/WindowManagerView.swift
@@ -9,31 +9,33 @@ import SwiftUI
 
 struct WindowManagerView: View {
     var body: some View {
-        HStack(alignment: .top, spacing: 9) {
-            OneClickButton(
-                labelTop: "Enter",
-                labelBottom: "Full Screen",
-                icon: "rectangle.inset.filled"
-            ) {
-                WindowManagerService.shared.toFullScreen()
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 9) {
+                OneClickButton(
+                    labelTop: "Enter",
+                    labelBottom: "Full Screen",
+                    icon: "rectangle.inset.filled"
+                ) {
+                    WindowManagerService.shared.toFullScreen()
+                }
+                
+                OneClickButton(
+                    labelTop: "Left",
+                    labelBottom: "Two Thirds",
+                    icon: "rectangle.leadinghalf.inset.filled"
+                ) {
+                    WindowManagerService.shared.toTwoThirdsLeft()
+                }
+                
+                OneClickButton(
+                    labelTop: "Right",
+                    labelBottom: "One Third",
+                    icon: "rectangle.trailingthird.inset.filled"
+                ) {
+                    WindowManagerService.shared.toOneThirdRight()
+                }
             }
-            
-            OneClickButton(
-                labelTop: "Left",
-                labelBottom: "Two Thirds",
-                icon: "rectangle.leadinghalf.inset.filled"
-            ) {
-                WindowManagerService.shared.toTwoThirdsLeft()
-            }
-            
-            OneClickButton(
-                labelTop: "Right",
-                labelBottom: "One Third",
-                icon: "rectangle.trailingthird.inset.filled"
-            ) {
-                WindowManagerService.shared.toOneThirdRight()
-            }
-        }
+        }.frame(width: 240, height: 66)
     }
 }
 
